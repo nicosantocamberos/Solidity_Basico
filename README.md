9883ee51760b76fe0647637a17f4ae78395a5a51
nicosantocamberos <nicosantocamberos@gmail.com>
2024-10-12 20:14:10 -0500

    Primer contrato

--- README.md
+++ README.md
@@ -10,3 +10,4 @@ Este es un cambio desde el "GittHub" ,**En su version web
 * Esto se puede hacer desde el IDE de REMIX
 * *Esto se puede hace en local desde una terminal
 * *Esto se puede hacer desde Visual Studio Code
+* Test 12/1024
---
+++ hola_mundo.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24; // Se define la versión del compilador
+
+contract hola_mundo {
+
+    string public saludo = "Hola desde educateth 2.0";// publica consultar fuera de la blochchain
+
+}
